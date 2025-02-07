dedca0262800a56e20966a37d6e257ad21ea8871
Román Cárdenas Rodríguez <rcardenas.rod@gmail.com>
2025-02-08 00:27:23 +0100

    Documentation for riscv-slic-macros

--- riscv-slic-macros/src/lib.rs
+++ riscv-slic-macros/src/lib.rs
@@ -6,7 +6,42 @@ mod export;
 mod input;
 mod swi;
 
-// Ex. codegen!(pac = <pac crate>, swi = [list, of, software, interrupts], backend = <backend-specific configuration>)
+/// Generates all the necessary code for creating a SLIC struct and its associated functions.
+///
+/// # Usage
+///
+/// ```ignore
+/// codegen!([slic = riscv_slic,] pac = <pac::crate>, swi = [list, of, software, interrupts][, backend = <backend-specific configuration>]);
+/// ```
+///
+/// # Arguments
+///
+/// * `slic` - Path to the SLIC crate. This is optional and defaults to `riscv_slic`.
+/// * `pac` - Path to the peripheral access crate for the target device. This is required.
+/// * `swi` - A list of software interrupts to handle. This is required, but can be an empty list.
+/// * `backend` - The backend-specific configuration. Depending on the backend, this may be required or optional.
+///
+/// # Backend-specific configuration
+///
+/// ## `clint-backend`
+///
+/// * `hart_id` - The identifier of the HART whose software interrupt should be triggered.
+///
+/// ### Example
+///
+/// ```ignore
+/// codegen!(pac = pac, swi = [list, of, software, interrupts], backend = { hart_id = H0 });
+/// ```
+///
+/// ## `mecall-backend`
+///
+/// This backend does not require any configuration.
+///
+/// ### Example
+///
+/// ```ignore
+/// codegen!(pac = pac, swi = [list, of, software, interrupts]);
+/// ```
 #[proc_macro]
 pub fn codegen(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as input::CodegenInput);
